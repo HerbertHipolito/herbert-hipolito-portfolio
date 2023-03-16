265f21d14d3462d32239f228b4767de6be5e9302
Herbert Hipólito <hipolito160@gmail.com>
2023-03-16 18:19:29 -0300

    grammatical mistakes removed

--- src/components/sections/section3/section3.js
+++ src/components/sections/section3/section3.js
@@ -6,32 +6,35 @@ export default function section3(){
     return <section id="section3">
 
         <p id="section3-title">Meus Projetos</p>
+
         
         <div className='section3-data-back-end'>    
             <div id="section3-buttons">
                 <button type="text" id='back-end-button'> Back-end </button>
             </div>
             <div id="my-projects-back">
+
                 <div className='project-class-back'>
                     <p className='project-title'>Flash-cards</p>
                     <p className='project-text'>Aplicação que auxilia o usuário no seu aprendizado de inglês por meio de Flash-cards.
                         O usuário pode cadastrar palavras por meio de link de youtube e selecionar as desejadas para poder revê-las.
                     </p>
-                    <a href='https://github.com/HerbertHipolito/flash-card-MERN'>Link do Github desse projeto</a>
+                    <a href='https://github.com/HerbertHipolito/flash-card-MERN'>Link do Github</a>
                 </div>    
                 <div className='project-class-back'>
                     <p className='project-title'>Blog</p>
                     <p className='project-text'>Um simples blog para o compartilhamento de opiniões e notícias.
                         Cada usuário pode cadastrar, remover e comentar blogs de outras pessoas.
                     </p>
-                    <a href='https://github.com/HerbertHipolito/my-blog'>Link do Github desse projeto</a>
+                    <a href='https://github.com/HerbertHipolito/my-blog'>Link do Github</a>
                 </div>    
                 <div className='project-class-back'>
                     <p className='project-title'>Registro de usuários</p>
                     <p className='project-text'>Aplicação simples feita com React e Node.js para simular um sistema de cadastro.</p>
-                    <a href='https://github.com/HerbertHipolito/Users-registration-page-using-React'>Link do Github desse projeto</a>
+                    <a href='https://github.com/HerbertHipolito/Users-registration-page-using-React'>Link do Github</a>
                 </div>    
             </div>
+
         </div>
 
         <div className='section3-data-back-end'>    
@@ -39,25 +42,27 @@ export default function section3(){
                 <button>C. de dados</button>
             </div>
             <div id="my-projects-back">
+
                 <div className='project-class-back'>
                     <p className='project-title-cc'>Classificação de nodulos Benignos e malignos</p>
-                    <p className='project-text'>Construção e uso de Rede Neurais Convulasionais para a classificação de nodulos benignos e malígnos com base em
+                    <p className='project-text'>Construção e uso de Rede Neurais Convulasionais para a classificação de nódulos benignos e malígnos com base em
                     imagens de tumografias de pulmão.
                     </p>
-                    <a href='https://github.com/HerbertHipolito/Classification-of-lung-nodules'>Link do Github desse projeto</a>
+                    <a href='https://github.com/HerbertHipolito/Classification-of-lung-nodules'>Link do Github</a>
                 </div>    
                 <div className='project-class-back'>
                     <p className='project-title-cc'>Predição da evapotranspiração de referência</p>
                     <p className='project-text'>Construção e uso de Rede Neurais multicamadas para estimação da Evapotranspiração de Referência para 16 
-                    estações metereolôgicas do Ceará utilizando temperatura como variável climatica.
+                    estações metereolôgicas do Ceará utilizando temperatura como variável climática.
                     </p>
-                    <a href='https://github.com/HerbertHipolito/prediction-evapotranspiration-'>Link do Github desse projeto</a>
+                    <a href='https://github.com/HerbertHipolito/prediction-evapotranspiration-'>Link do Github</a>
                 </div>    
                 <div className='project-class-back'>
-                    <p className='project-title-cc'>Uso de heuristicas para solução do Caixeiro viajante</p>
+                    <p className='project-title-cc'>Uso de heurísticas para solução do Caixeiro viajante</p>
                     <p className='project-text'>Implementação das principais heuristicas para a solução do problema do Caixeiro-viajante, no caso, Greedy, Hill Climbing, VND, RMS e GRASP</p>
-                    <a href='https://github.com/HerbertHipolito/Travelling-Salesman'>Link do Github desse projeto</a>
-                </div>    
+                    <a href='https://github.com/HerbertHipolito/Travelling-Salesman'>Link do Github</a>
+                </div>  
+
             </div>
         </div>
 
