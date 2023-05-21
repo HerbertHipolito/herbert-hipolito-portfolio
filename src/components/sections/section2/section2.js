1471943395dd92c75ddfda5b6ed356ce7512991c
Herbert Hipólito <hipolito160@gmail.com>
2023-05-21 10:05:02 -0300

    portfolio pdf and intro text updated

--- src/components/sections/section2/section2.js
+++ src/components/sections/section2/section2.js
@@ -4,15 +4,15 @@ import React from 'react';
 
 export default function Section2(){
     return <section id="section2">
-        
+
         <p id='section2-title'>Um pouco sobre mim</p>
 
         <div id="section2-elements">
             <p id="section2-main-text">
-            &nbsp;&nbsp;&nbsp; Amante de tecnologias, estou sempre procurando aprender e evoluir meus conhecimentos. 
+            &nbsp;&nbsp;&nbsp; Amante de tecnologias, estou sempre procurando aprender e evoluir meus conhecimentos.
             Gosto de desafios e de resolver problemas, tanto da área da matemática quanto de programação, procurando sempre a solução mais viável.<br/>
-            &nbsp;&nbsp;&nbsp; Atualmente estou no Mestrado em Métodos Quantitativos com ênfase em Inteligência Computacional na Universidade Federal do Ceará (UFC), 
-            tenho uma base bem forte em matemática, estatística e computação, o que me fez estudar sobre a área de Ciência de dados e Machine Learning, inclusive sendo tema para o meu TCC. Também 
+                &nbsp;&nbsp;&nbsp; Formado em Matemática Industrial pela Universidade Federal do Ceará (UFC) e atualmente procurando uma vaga relacionada a <span>Ciência de Dados</span> e/ou <span>Machine Learning</span>.
+            tenho uma base bem forte em matemática, estatística e computação, o que me fez estudar sobre a área de Ciência de dados e Machine Learning, inclusive sendo tema para o meu TCC. Também
             tenho uma grande afinidade para área de desenvolvimento web (FULL-STACK), mais especificamente com Node.js e MongoDB no BACK-END e ReactJS e CSS no FRONT-END.
             Alguns dos projetos que desenvolvi de ambas áreas estão disponíveis no meu Github!
             </p>
